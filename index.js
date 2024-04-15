bd7ae448d82a378f691b0788893c10cf8187fae4
CapaBlanca <blancacapa904@gmail.com>
2024-04-15 16:13:35 -0600

    se cambio el url de la BD

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import routes from "./routes/Datos.routes";
 import mongoose from "mongoose";
+const port = process.env.PORT || 4000;
+
 
 // const app = express();
 
@@ -39,7 +41,8 @@ const app = express();
 
 //conexion a base de datos
 mongoose.Promise=global.Promise;
-const dbUrl='mongodb://localhost:27017/netflix';
+const dbUrl='mongodb+srv://blancacapa904:Pk5F1WulCKdLt47P@cluster0.vebeflb.mongodb.net/';
+
 mongoose.connect(dbUrl)
 .then(mongoose=>console.log('Conectado a la bd en el puerto 27017'));
 //Listening de puertos
@@ -52,11 +55,15 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.use("/api",routes);
+app.use(cors());
 
-app.listen(app.get('port'), () => {
-  console.log(`Example app listening on port:` + app.get('port'));
+// app.listen(app.get('port'), () => {
+//   console.log(`Example app listening on port:` + app.get('port'));
+// })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
-
 
 
 
