118348856951c0502c9d8d5e6d3c083d06bd1c71
CapaBlanca <blancacapa904@gmail.com>
2024-04-19 18:42:50 -0600

    se agrego multer

--- controllers/Datos.controller.js
+++ controllers/Datos.controller.js
@@ -3,10 +3,8 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '..', 'uploads')); 
-   },
-
-
+    cb(null, 'uploads');
+  },
   filename: function (req, file, cb) {
     cb(null, `${Date.now()}-${file.originalname}`);
   }
