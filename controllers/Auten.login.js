1ab05e0962173597e2de1c3c758dbdacfe6dbb30
CapaBlanca <blancacapa904@gmail.com>
2024-04-25 11:18:56 -0600

    cambios

---
+++ controllers/Auten.login.js
@@ -0,0 +1,39 @@
+// En tu controlador de autenticación (AuthController.js)
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import User from '../models/index.js';
+
+const AuthController = {
+  login: async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+      // Buscar al usuario en la base de datos por nombre de usuario
+      const user = await Cuentas.findOne({ userame });
+
+      // Si no se encuentra el usuario, devolver un error
+      if (!user) {
+        return res.status(401).json({ message: 'Credenciales inválidas' });
+      }
+
+      // Verificar la contraseña
+      const isPasswordValid = await bcrypt.compare(password, Cuentas.password);
+
+      // Si la contraseña no coincide, devolver un error
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: 'Credenciales inválidas' });
+      }
+
+      // Generar un token de autenticación
+      const token = jwt.sign({ userId: user._id }, 'tu-secreto-seguro', { expiresIn: '1h' });
+
+      // Devolver el token como respuesta
+      res.status(200).json({ token });
+    } catch (error) {
+      console.error('Error de autenticación:', error);
+      res.status(500).json({ message: 'Error de autenticación' });
+    }
+  }
+};
+
+export default AuthController;
--- routes/Cuentas,routes.js
+++ routes/Cuentas,routes.js
@@ -13,6 +13,8 @@ router.patch("/put/:id", Datosctr.putDatos); // PATCH /Movies/:id para actualiza
 router.delete("/delet/:id",Datosctr.delDatos); // DELETE /Movies/:id para eliminar un dato por su ID
 
 
+router.post("/login", AuthController.login); 
+
 
 
 export default router;
