ebb0bb3ecc6a107a2fb791aece03d6dbb0415203
CapaBlanca <blancacapa904@gmail.com>
2024-04-24 23:07:51 -0600

    se agrego fetch

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ import CuentasR from "./Cuentas,routes.js";
 const router = routerx();
 
 router.use("/movies",DatosR);//hacer peticiones
-router.use("/cuenta",CuentasR);//hacer peticiones
+router.use("/cuentas",CuentasR);//hacer peticiones
 
 
 export default router;
