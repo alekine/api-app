6fe7cbaa7fd336320149015ee77d93b0dbac1279
CapaBlanca <blancacapa904@gmail.com>
2024-05-09 08:54:03 -0600

    se cambiaron rutas

--- routes/resenas.routes.js
+++ routes/resenas.routes.js
@@ -10,7 +10,7 @@ router.post("/addResena", Datosctr.postResena); // POST /Movies para agregar nue
 router.get("/obtenerResena", Datosctr.getResena); // GET /Movies para obtener todos los datos
 
 
-router.get("/obtener/:id", Datosctr.getResena); // GET /Movies/:id para obtener un dato por su ID
+router.get("resena/obtener/:id", Datosctr.getResena); // GET /Movies/:id para obtener un dato por su ID
 router.patch("/put/:id", Datosctr.putResena); // PATCH /Movies/:id para actualizar un dato por su ID
 router.delete("/delet/:id",Datosctr.delResena); // DELETE /Movies/:id para eliminar un dato por su ID
 
