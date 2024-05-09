52c9c289dc98e7a771d4177b9a78b0765df8e28e
CapaBlanca <blancacapa904@gmail.com>
2024-05-09 08:04:05 -0600

    se cambiaron rutas

--- routes/Datos.routes.js
+++ routes/Datos.routes.js
@@ -6,7 +6,7 @@ import routerx from "express-promise-router";
 
 const router = routerx();
 
-router.post("/agregar", Datosctr.postDatos); // POST /Movies para agregar nuevos datos
+router.post("/post", Datosctr.postDatos); // POST /Movies para agregar nuevos datos
 router.get("/obtenerMovie", Datosctr.getDatos); // GET /Movies para obtener todos los datos
 router.get("/obtener/:id", Datosctr.getDato); // GET /Movies/:id para obtener un dato por su ID
 router.patch("/put/:id", Datosctr.putDatos); // PATCH /Movies/:id para actualizar un dato por su ID
