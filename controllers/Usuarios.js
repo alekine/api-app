16dc16705e5d6a5add33aef9d9eb225c7c4de651
CapaBlanca <blancacapa904@gmail.com>
2024-05-12 20:35:50 -0600

    se establecieron las rutas para usuario

--- controllers/Usuarios.js
+++ controllers/Usuarios.js
@@ -2,17 +2,22 @@ import User from "../models/index.js"
 
 export default{
     obtenerUserLogued: async (req, res) => {
-        const {usuario_id} = req.usuario;
 
-        const response = await User.Cuentas.findById(usuario_id);
-
-        if(!response){
-            res.status(400).send({
-                msg: "No existe el usuario"
-            })
-        }else{
-            res.status(200).send({response});
-        }
-    }
+       res.status(200).send({msg: "estamos en la obtencion de los datos"})
 }
 
+}
+
+
+// const {usuario_id} = req.usuario;
+
+// const response = await User.Cuentas.findById(usuario_id);
+
+// if(!response){
+//     res.status(400).send({
+//         msg: "No existe el usuario"
+//     })
+// }else{
+//     res.status(200).send({response});
+// }
+// }
